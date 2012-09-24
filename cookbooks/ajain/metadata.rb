5e66db0c15021720c97ceba353b38b5784b4a0fa
amitjain1975 <amit.jain@personalcapital.com>
2012-09-24 20:49:36 +0000

    new commit

--- cookbooks/ajain/metadata.rb
+++ cookbooks/ajain/metadata.rb
@@ -1,6 +1,9 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
+maintainer       "Personal Capital"
+maintainer_email "amit.jain@personalcapital.com"
 license          "All rights reserved"
 description      "Installs/Configures ajain"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+
+recipe "ajain::helloworld","Prints hello world"
+
